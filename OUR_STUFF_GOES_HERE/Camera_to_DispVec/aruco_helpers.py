82a0faa035e074f0ea05290502657add23c2396b
SpeedyTurtle599 <SpeedyTurtle599@users.noreply.github.com>
2024-05-28 22:22:52 -0500

    renamed that godawful folder; OH MY GOD IT'S WORKING

--- OUR_STUFF_GOES_HERE/Camera_to_DispVec/aruco_helpers.py
+++ OUR_STUFF_GOES_HERE/Camera_to_DispVec/aruco_helpers.py
@@ -3,13 +3,13 @@ import cv2
 import math
 
 def CreateDetector():
-    #setup Aruco Detector and which marker library see https://chev.me/arucogen/
+    # set up Aruco Detector and which marker library see https://chev.me/arucogen/
     arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50) # we will use marker 0 of the dictionary, 50 cm^2 as of rn
     arucoParams = cv2.aruco.DetectorParameters() # use default detect params
     detector = cv2.aruco.ArucoDetector(arucoDict,arucoParams) #define detector object
     return(arucoDict,arucoParams,detector)
 
-def GetRelativeYaw(corners): #calcs only for the corners of 1st aruco, i.e. with lowest id # in set
+def GetRelativeYaw(corners): # calcs only for the corners of 1st aruco, i.e. with lowest id # in set
     x1= int(corners[0][0][0][0])
     y2= int(corners[0][0][0][1])
     x2= int(corners[0][0][1][0])
@@ -21,4 +21,3 @@ def GetRelativeYaw(corners): #calcs only for the corners of 1st aruco, i.e. with
     angle = ((math.atan2((y2-y1),(x2-x1)))*180)/math.pi
     
     return(angle)
-
